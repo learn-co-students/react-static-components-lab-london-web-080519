0c35fe628f95a5a189fc912cc9a8bda6c018a7c9
github username <aude53@gmail.com>
2019-10-07 09:17:52 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import CatComponent from './CatComponent.js';
 import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
 import MouseComponent from './MouseComponent';
+import { exists } from 'fs';
 
 class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
 			<div className="App">
+				<CatComponent/>
+				<EinsteinQuoteComponent/>
+				<MouseComponent/>
 			</div>
 		);
 	}
 }
+
+// class CatComponent extends App {
+// 	render() {
+
+// 	}
+// }
+
+// class EinsteinQuoteComponent extends App {
+// 	render() {
+
+// 	}
+// }
+
+// class MouseComponent extends App {
+// 	render() {
+
+// 	}
+// }
 
 export default App;
